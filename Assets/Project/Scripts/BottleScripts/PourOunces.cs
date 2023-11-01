70bdd15c2020fe15a4d2157a5ea449a2e326f739
Marcos <marcos.ruiz@enti.cat>
2023-11-01 21:17:32 +0100

    Fixing Shaker

--- Assets/Project/Scripts/BottleScripts/PourOunces.cs
+++ Assets/Project/Scripts/BottleScripts/PourOunces.cs
@@ -6,13 +6,14 @@ public class PourOunces : MonoBehaviour
 {
     private Bottle bottle;
 
-    [SerializeField]
-    private OuncesCounter ouncesCounter;
+    //[SerializeField]
+    //private OuncesCounter ouncesCounter;
 
-    [SerializeField]
     private Drink shaker;
 
     private BoxCollider2D boxCollider;
+
+    private GameObject shakerObject;
 
     [SerializeField]
     private BoxCollider2D shakerCollider;
@@ -20,7 +21,17 @@ public class PourOunces : MonoBehaviour
     private void Awake()
     {
         bottle = GetComponent<Bottle>();
-        boxCollider = GetComponent<BoxCollider2D>();
+        boxCollider = GetComponent<BoxCollider2D>();     
+    }
+
+    private void Update()
+    {
+        if(shakerObject!= null) 
+        {
+            shakerObject = GameObject.Find("Shaker");
+            shakerCollider = shakerObject.GetComponent<BoxCollider2D>();
+            shaker = shakerObject.GetComponent<Drink>();
+        }
     }
 
     private void OnMouseUp()
@@ -29,7 +40,7 @@ public class PourOunces : MonoBehaviour
         {
             shaker.AddOunce(bottle.GetTypeOfOunces()[0]);
             bottle.SubstractOneOunce();
-            ouncesCounter.AddOneToCounter();
+            //ouncesCounter.AddOneToCounter();
         }
     }
 }
