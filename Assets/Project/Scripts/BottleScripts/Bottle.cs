8680f816839c0f6ab1e1fa92aa24cf52c1421cb6
PauG <pau.giro@enti.cat>
2023-11-04 16:18:33 +0100

    BottleAnimationFinished

--- Assets/Project/Scripts/BottleScripts/Bottle.cs
+++ Assets/Project/Scripts/BottleScripts/Bottle.cs
@@ -4,23 +4,18 @@ using UnityEngine;
 
 public class Bottle : Drink
 {
-    private SpriteRenderer spriteRenderer;
-    [SerializeField] private Sprite[] sprites;
-    private int currentSprite;
+    private LiquidAnimation liquidAnimation;
 
     private void Awake()
     {
-        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
-        sprites = new Sprite[10];
-        currentSprite = 0;
-        spriteRenderer.sprite = sprites[0];
-
         for (int i = 0; i < maxOunces; i++)
         {
             ouncesInDrink.Add(typeOfDrink);
         }
         currentOunces = maxOunces;
         isFull = true;
+
+        liquidAnimation = GetComponentInChildren<LiquidAnimation>();
     }
 
     public void RefillOunces()
@@ -31,10 +26,6 @@ public class Bottle : Drink
     public void SubstractOneOunce()
     {
         currentOunces--;
-        currentSprite++;
-        if(currentSprite > 9)
-            spriteRenderer.sprite = null;
-        else
-            spriteRenderer.sprite = sprites[currentSprite];
+        liquidAnimation.SetAnimation(currentOunces);
     }
 }
