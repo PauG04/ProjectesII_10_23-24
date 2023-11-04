8f6a3139f5199ab4de998f544b0fb5f356d42832
PauG <pau.giro@enti.cat>
2023-11-04 02:38:05 +0100

    AssetsImported

--- Assets/Project/Scripts/BottleScripts/Bottle.cs
+++ Assets/Project/Scripts/BottleScripts/Bottle.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class Bottle : Drink
 {
-    private void Start()
+    private SpriteRenderer spriteRenderer;
+    [SerializeField] private Sprite[] sprites;
+    private int currentSprite;
+
+    private void Awake()
     {
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        sprites = new Sprite[10];
+        currentSprite = 0;
+        spriteRenderer.sprite = sprites[0];
+
         for (int i = 0; i < maxOunces; i++)
         {
             ouncesInDrink.Add(typeOfDrink);
@@ -22,5 +31,10 @@ public class Bottle : Drink
     public void SubstractOneOunce()
     {
         currentOunces--;
+        currentSprite++;
+        if(currentSprite > 9)
+            spriteRenderer.sprite = null;
+        else
+            spriteRenderer.sprite = sprites[currentSprite];
     }
 }
