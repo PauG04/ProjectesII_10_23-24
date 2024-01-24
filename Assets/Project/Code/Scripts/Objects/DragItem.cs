8e9ace282fee90f3066bb008114af51259c80559
Marcos <marcos.ruiz@enti.cat>
2024-01-24 16:39:10 +0100

    Double lerp

--- Assets/Project/Code/Scripts/Objects/DragItem.cs
+++ Assets/Project/Code/Scripts/Objects/DragItem.cs
@@ -10,6 +10,9 @@ public class DragItem : MonoBehaviour
 
     private Vector3 initScale;
 
+    private bool firstLerp;
+    private bool secondLerp;
+
     [Header("Tranform Vairables")]
     [SerializeField] private float increaseScale;
 
@@ -18,13 +21,16 @@ public class DragItem : MonoBehaviour
     [SerializeField] private bool hasToBeDestroy;
 
     [Header("Lerp Variables")]
-    [SerializeField] private float velocity;
+    [SerializeField] private float velocityX;
+    [SerializeField] private float velocityY;
 
     private void Start()
     {
         targetJoint = GetComponent<TargetJoint2D>();
         isInWorkSpace = false;
         initScale = transform.localScale;
+        firstLerp = false;
+        secondLerp = false;
     }
 
     private void Update()
@@ -45,12 +51,34 @@ public class DragItem : MonoBehaviour
     {
         if (!dragging && !isInWorkSpace)
         {
-            transform.position = Vector2.Lerp(transform.position, parent.transform.position, Time.deltaTime * velocity);
-        }
-        if(transform.position.x > parent.transform.position.x - 0.1 && transform.position.x < parent.transform.position.x + 0.1 && hasToBeDestroy)
-        {
-            Destroy(gameObject);
-        }
+            if(firstLerp)
+            {
+                Vector3 newPosition = transform.localPosition;
+                newPosition.x = Mathf.Lerp(transform.position.x, parent.transform.position.x, Time.deltaTime * velocityX);
+
+                transform.position = newPosition;
+            }
+            if (transform.position.x > parent.transform.position.x - 0.02 && transform.position.x < parent.transform.position.x + 0.02)
+            {
+                firstLerp = false;
+                secondLerp = true;
+            }
+            if(secondLerp)
+            {
+                Vector3 newPosition = transform.localPosition;
+                newPosition.y = Mathf.Lerp(transform.position.y, parent.transform.position.y, Time.deltaTime * velocityY);
+
+                transform.position = newPosition;
+            }
+            if (transform.position.y > parent.transform.position.y - 0.02 && transform.position.y < parent.transform.position.y + 0.02)
+            {
+                secondLerp = false;
+                if(hasToBeDestroy)
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }      
     }
 
     private void OnTriggerExit2D(Collider2D collision)
@@ -73,11 +101,14 @@ public class DragItem : MonoBehaviour
     private void OnMouseDown()
     {
         dragging = true;
+        firstLerp = false;
+        secondLerp = false;
     }
 
     private void OnMouseUp()
     {
-        dragging = false;       
+        dragging = false;
+        firstLerp = true;
     }
 }
 
