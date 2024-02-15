0b02a2d3bb58f60778ba7efe37f4edf7521468be
PauG <pau.giro@enti.cat>
2024-02-15 23:24:54 +0100

    HotFix

--- Assets/Project/Code/Scripts/Liquid/LiquidManager.cs
+++ Assets/Project/Code/Scripts/Liquid/LiquidManager.cs
@@ -8,9 +8,26 @@ public class LiquidManager : MonoBehaviour
     [SerializeField] private float currentLiquid = 0;
     private Dictionary<DrinkNode.Type, int> particleTypes;
 
+    [SerializeField] private bool isGlass;
+    private DragItemsNew dragItems;
+
     private void Awake()
     {
         particleTypes = new Dictionary<DrinkNode.Type, int>();
+
+        if (isGlass)
+            dragItems = GetComponentInParent<DragItemsNew>();
+    }
+
+    private void Update()
+    {
+        if (isGlass)
+        {
+            if (particleTypes.Count > 0)
+                dragItems.SetHasToReturn(false);
+            else
+                dragItems.SetHasToReturn(true);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
