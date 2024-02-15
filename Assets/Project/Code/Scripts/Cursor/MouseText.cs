6e416d7206ffbe967b4947939b1abe4a5542d595
PAUGIROGARCIA <pau.giro@enti.cat>
2024-02-15 17:57:34 +0100

    Create CursorBox

---
+++ Assets/Project/Code/Scripts/Cursor/MouseText.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MouseText : MonoBehaviour
+{
+    [SerializeField] private GameObject box;
+    private TextMeshPro textMP;
+
+    private void Awake()
+    {
+        textMP = GetComponentInChildren<TextMeshPro>();
+    }
+
+    private void Update()
+    {
+        transform.position = CursorManager.instance.GetCursorPosition();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<RotateBottle>(out RotateBottle bottle))
+        {
+            box.SetActive(true);
+            textMP.text = collision.gameObject.name;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<RotateBottle>(out RotateBottle bottle))
+        {
+            box.SetActive(false);
+        }
+    }
+
+
+
+}
