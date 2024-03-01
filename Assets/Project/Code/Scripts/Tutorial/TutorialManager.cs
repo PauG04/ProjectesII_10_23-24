9e26c29d64b4611875e2fc86903b1ec2f889c474
Marcos <marcos.ruiz@enti.cat>
2024-03-01 02:41:13 +0100

    Fixing Build problems

--- Assets/Project/Code/Scripts/Tutorial/TutorialManager.cs
+++ Assets/Project/Code/Scripts/Tutorial/TutorialManager.cs
@@ -4,7 +4,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 using TMPro;
-using TreeEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 using static UnityEngine.Rendering.DebugUI;
