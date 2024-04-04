704fb231656550c51edf21d67c2d4a2b3a25c365
PauGi <pau.giro@enti.cat>
2024-04-04 02:14:07 +0200

    Added news to Wiki

--- Assets/Project/Code/Scripts/Wiki/CocktailPage.cs
+++ Assets/Project/Code/Scripts/Wiki/CocktailPage.cs
@@ -1,35 +1,21 @@
 using TMPro;
 using UnityEngine;
 
-public class WikiPage : MonoBehaviour
+public class CocktailPage : MonoBehaviour
 {
-    [SerializeField] private bool isLeft;
-
+    [SerializeField] private CocktailNode cocktail;
     [SerializeField] private TextMeshPro nameText;
     [SerializeField] private TextMeshPro subtitleText;
     [SerializeField] private TextMeshPro descriptionText;
     [SerializeField] private SpriteRenderer spriteRenderer;
     [SerializeField] private RectTransform turnPage;
 
-    public void UpdatePage(CocktailNode cocktail)
+    public void InitPage()
     {
         nameText.text = cocktail.cocktailName;
         int integerPart = (int)cocktail.price;
         subtitleText.text = integerPart.ToString() + "'99 €";
         descriptionText.text = cocktail.description;
         spriteRenderer.sprite = cocktail.sprite;
-    }
-
-    public void ClearPage()
-    {
-        nameText.text = "";
-        subtitleText.text = "";
-        descriptionText.text = "";
-        spriteRenderer.sprite = null;
-    }
-
-    public bool GetIsLeft()
-    {
-        return isLeft;
     }
 }
